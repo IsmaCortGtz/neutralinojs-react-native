9df19a6bc6f51443fa1d0a2daeb8a0880d0d2ed4
IsmaCortGtz <ismacortgtz@gmail.com>
2025-11-09 15:51:31 -0600

    Init neutralino lib by default

--- index.neu.js
+++ index.neu.js
@@ -1,8 +1,11 @@
 import '..';
 import { AppRegistry } from 'react-native';
+import { init } from '@neutralinojs/lib';
 import { name as appName } from '../app.json';
 
 AppRegistry.runApplication(appName, {
   initialProps: {},
   rootTag: document.getElementById('root'),
 });
+
+init();
